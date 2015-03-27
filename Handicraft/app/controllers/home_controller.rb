e9024c1e8d9cba8df9b5f2a254ec92a2be91445e
bharatismir <bharatisamir@gmail.com>
2015-03-27 11:42:39 -0400

    set root to home page
    modified view to display trending items, viewing history, best selling items
    
    modified model to provide data for views

--- Handicraft/app/controllers/home_controller.rb
+++ Handicraft/app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    date_param=Time.now-86400
+    #date_param=1.day.from_now
+    special_date_param=1.day.from_now
+    @specialItems=Product.where("created_at = '#{special_date_param}'")
+    @trendingItems=History.where("date_visited>= '#{date_param}'")
+    # @bestSellers=Order.select("product_id").group("product_id").count("product_id").order("quantity DESC").limit(10)
   end
 end
