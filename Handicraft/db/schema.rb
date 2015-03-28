96bbb4b6753fc2af7aea36c68aafafe973a0e780
manish bajracharya <mbajra1@students.towson.edu>
2015-03-27 22:20:04 -0400

    update the gem bcrypt Overridden the bin/rails file to solve the conflict

--- Handicraft/db/schema.rb
+++ Handicraft/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327161417) do
+ActiveRecord::Schema.define(version: 20150328015512) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "customer_id"
@@ -36,8 +36,21 @@ ActiveRecord::Schema.define(version: 20150327161417) do
     t.datetime "updated_at",   null: false
   end
 
-# Could not dump table "products" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "products", force: :cascade do |t|
+    t.string   "product_id"
+    t.string   "name"
+    t.text     "description"
+    t.string   "image_url"
+    t.string   "category"
+    t.string   "subcategory"
+    t.string   "product_condition"
+    t.decimal  "price",                 precision: 8, scale: 2
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
+    t.string   "seller_id"
+    t.string   "technical_description"
+    t.integer  "quantity"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
